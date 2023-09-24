9b7f3fb54694a91dd088e58b5aa1ac7b77d261bc
ZheerBarzan <zheerbarzan85@outlook.com>
2023-09-24 20:02:44 +0300

    making the HTML of the application

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <main>
+    <div class="container">
+      <header>
+        <h1>Notes</h1>
+        <button>+</button>
+      </header>
+
+      <div class="cards-conatiner">
+      <div class="card">
+      <p class="main-text"></p>
+      <p class="date"></p>
+      </div>
+      
+      </div>
+    </div>
+  </main>
+</template>
+
+
+<style scoped>
+
+main{
+  height: 100vh;
+  width: 100vw;
+}
+
+
+
+
+
+</style>
